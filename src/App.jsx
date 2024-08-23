3ce297c64a9d53e81946c55b7ecce5646782efef
Daniel <dbeaulne1@gmail.com>
2024-08-23 13:40:52 -0400

    create homepage files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.scss';
+import "./App.scss";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// Pages
+import HomePage from "./pages/HomePage/HomePage.jsx";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return <div className="App"></div>;
 }
 
 export default App;
