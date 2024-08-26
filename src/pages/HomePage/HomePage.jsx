775bfe8b60a8912ab563c060769d4e34840a4f83
Daniel <dbeaulne1@gmail.com>
2024-08-26 19:56:47 -0400

    create Hero component and style accordingly

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,17 @@
 /* Home Page */
 
 import "./HomePage.scss";
+import Hero from "../../components/Hero/Hero";
 import heroImg from "../../assets/images/chewy-gWzTum_yMCg-unsplash.jpg";
 
 const HomePage = () => {
 	return (
 		<div className="homepage">
-			<h1>hello</h1>
+			<Hero
+				title="Pet care, your way and on your schedule"
+				subtitle="Connect with trusted sitters"
+				src={heroImg}
+			/>
 		</div>
 	);
 };
