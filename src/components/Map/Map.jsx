87cb34da8abdd6f4fa9a9d0e01918fa8075bd027
Daniel <dbeaulne1@gmail.com>
2024-09-06 12:08:39 -0400

    complete and style hard-coded results component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,7 @@
 /* Map component */
 import "./Map.scss";
 import MapImage from "../../assets/images/map.png";
+import MapImage_2 from "../../assets/images/map-2.png";
 
 const Map = () => {
 	return (
@@ -9,7 +10,7 @@ const Map = () => {
 			id="map">
 			<img
 				className="mapImage"
-				src={MapImage}
+				src={MapImage_2}
 				alt="map overview of Courtice, Ontario"
 			/>
 		</div>
--- src/pages/ResultsPage/ResultsPage.jsx
+++ src/pages/ResultsPage/ResultsPage.jsx
@@ -1,11 +1,13 @@
 /* Results page component */
 import "./ResultsPage.scss";
 import Map from "../../components/Map/Map";
+import Results from "../../components/Results/Results";
 
 const ResultsPage = () => {
 	return (
-		<section className="results">
+		<section className="results-page">
 			<Map />
+			<Results />
 		</section>
 	);
 };
